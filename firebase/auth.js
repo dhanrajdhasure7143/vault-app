895c6b86d18a376ffbc5d9b1f601415179e8f5e5
Dhanraj Deelip Hasure <dhanraj.hasure@epsoftinc.com>
2024-04-15 14:10:31 +0530

    names added in the top of the page

--- firebase/auth.js
+++ firebase/auth.js
@@ -1,11 +1,11 @@
 function signOut() {
-    firebase.auth().signOut().then(function() {
+    firebase.auth().signOut().then(function () {
         console.log("Signed out");
         displayNotification("Signed Out!!")
         document.getElementById('login-form').style.display = 'block';
         document.getElementById('file-upload').style.display = 'none';
-    }).catch(function(error) {
-        displayNotification(error.message)           
+    }).catch(function (error) {
+        displayNotification(error.message)
 
     });
 }
@@ -22,17 +22,25 @@ function login() {
     var password = document.getElementById('login-password').value;
 
     firebase.auth().signInWithEmailAndPassword(email, password)
-        .then(function(userCredential) {
+        .then(function (userCredential) {
             var user = userCredential.user;
-            console.log('Logged in:', user);
+            localStorage.setItem('users-mail', user.email)
+            console.log('Logged in email Id :', localStorage.getItem('users-mail'));
+
+            var namedElement = document.getElementById('named');
+            if (namedElement) {
+                namedElement.innerHTML = localStorage.getItem('users-mail');
+            } else {
+                displayNotification("Element with ID 'named' not found!");
+            }
             document.getElementById('file-upload').style.display = 'block';
             document.getElementById('login-form').style.display = 'none';
         })
-        .catch(function(error) {
+        .catch(function (error) {
             var errorCode = error.code;
             var errorMessage = error.message;
             console.error('Login error:', errorCode, errorMessage);
-            displayNotification('Login failed, plz try again!')           
+            displayNotification('Login failed, plz try again!')
         });
 }
 
@@ -42,17 +50,18 @@ function signup() {
     var password = document.getElementById('signup-password').value;
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(function(userCredential) {
+        .then(function (userCredential) {
             var user = userCredential.user;
+            localStorage.setItem('users-mail', user.email)
             console.log('Signed up:', user);
             displayNotification('Signup done!')
             document.getElementById('file-upload').style.display = 'block';
             document.getElementById('signup-form').style.display = 'none';
         })
-        .catch(function(error) {
+        .catch(function (error) {
             var errorCode = error.code;
             var errorMessage = error.message;
-            displayNotification(error.message)           
+            displayNotification(error.message)
         });
 }
 
@@ -61,7 +70,7 @@ function displayNotification(message) {
     notificationOverlay.textContent = message;
     notificationOverlay.classList.add("show");
 
-    setTimeout(function() {
+    setTimeout(function () {
         notificationOverlay.classList.remove("show");
     }, 3000);
 }
